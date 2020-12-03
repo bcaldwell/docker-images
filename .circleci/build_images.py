c64e263a04dcfb586c7be74efbc39b2731e0314a
Benjamin Caldwell <b.caldwell@salesforce.com>
2020-12-02 23:18:07 -0500

    add postgres_s3_backup

---
+++ .circleci/build_images.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import yaml
+import os
+
+def main():
+    images_file = "./images.yaml"
+
+    try:
+        images = yaml.load(open(images_file).read(), Loader=yaml.FullLoader)
+    except Exception as e:
+        print("failed to laod and parse yaml file", e)
+        return
+
+    for k, v in images.items():
+        image = {
+            "name": k
+        }
+
+        if isinstance(v,dict):
+            if "path" not in v:
+                print("not path property found for image ", k)
+            image["path"] = v["path"]
+            image["version"] = v["version"] if "version" in v else "latest"
+        elif isinstance(v, str):
+            image["path"] = v
+            image["version"] = "latest"
+        else:
+            print(v, "is an unsupported type")
+
+        build_image(image)
+
+def build_image(image):
+    changed = os_run("ci-scripts git/files_changed --git.files_changed.prefix {}".format(image["path"]))
+    if not changed:
+        print("no changes in", image["name"], image["path"])
+        return
+
+    os_run("ci-scripts docker/build_and_push_image --docker.images.dockerRepo {} --docker.images.folder {} --docker.tags \"{}, _sha, latest\"".format(image["name"], image["path"], image["version"]))
+    print()
+
+def os_run(cmd):
+    print(cmd)
+    return os.system(cmd)
+
+main()
---
+++ postgres_s3_backup/postgres_s3_backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DUMP_FILE=${DUMP_FILE:-/backup_`date +%Y%m%d_%H%M%S`.pgdump}
+PGPASSWORD=$POSTGRES_PASSWORD pg_dump -d $POSTGRES_DB -U $POSTGRES_USER -h $POSTGRES_HOST -f $DUMP_FILE
+
+bzip2 -z ${DUMP_FILE}
+
+echo $GPG_PUBLIC_KEY | base64 -d | gpg --import
+echo $GPG_OWNER_TRUST | base64 -d | gpg --import-ownertrust
+gpg --output ${DUMP_FILE}.bz2.gpg -e -r ${GPG_RECIPIENT} ${DUMP_FILE}.bz2
+
+AWS_ARGS=""
+if [[ ! -z "${S3_ENDPOINT_URL}" ]]; then
+  AWS_ARGS="--endpoint-url=${S3_ENDPOINT_URL}"
+fi
+
+aws s3 cp ${AWS_ARGS} ${DUMP_FILE}.bz2.gpg $S3_BACKUP_PATH
