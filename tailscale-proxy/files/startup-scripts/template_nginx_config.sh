8d975946834d93069e29a8c2e41d43e31ccb3473
Benjamin Caldwell <benjamin@bcaldwell.ca>
2021-10-31 15:26:54 -0700

    fix streaming in nginx image

--- tailscale-proxy/files/startup-scripts/template_nginx_config.sh
+++ tailscale-proxy/files/startup-scripts/template_nginx_config.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-for i in `find /etc/nginx/templates -type f -iname "*.tpl"`; do
+for i in `find /etc/nginx/templates-tcpconf.d/ -type f -iname "*.tpl"`; do
   file=$(basename $i)
   echo "Templating $i"
-  gomplate -f $i -o "/etc/nginx/conf.d/${file%.*}"
+  gomplate -f $i -o "/etc/nginx/tcpconf.d/${file%.*}"
 done
