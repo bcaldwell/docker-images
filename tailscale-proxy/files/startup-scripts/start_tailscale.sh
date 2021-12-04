437c4021e180ed880df1e6ea0eca0cf759ea938f
Benjamin Caldwell <8838361+bcaldwell@users.noreply.github.com>
2021-12-04 10:35:55 -0800

    Update start_tailscale.sh

--- tailscale-proxy/files/startup-scripts/start_tailscale.sh
+++ tailscale-proxy/files/startup-scripts/start_tailscale.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 tailscaled --state=/var/lib/tailscale/tailscaled.state --socket=/var/run/tailscale/tailscaled.sock &
-until tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=tailscale-proxy --accept-routes
+until tailscale up --authkey=${TAILSCALE_AUTHKEY} --hostname=${TAILSCALE_HOSTNAME:-tailscale-proxy} --accept-routes
 do
     sleep 0.1
 done
